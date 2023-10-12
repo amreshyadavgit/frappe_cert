6576cca64a70661b533d879ffaa88384bc51122d
amreshyadavgit <ami92.raj@gmail.com>
2023-10-12 11:34:22 +0530

    day3 tasks

--- airplane_mode/airplane_mode/doctype/flight_passenger/flight_passenger.py
+++ airplane_mode/airplane_mode/doctype/flight_passenger/flight_passenger.py
@@ -4,10 +4,7 @@
 # import frappe
 from frappe.model.document import Document
 
-class FlightPassenger(Document):
 
-    def before_save(self):
-        if self.last_name:
-            self.full_name = self.first_name + " " + self.last_name
-        else:
-            self.full_name = self.first_name
+class FlightPassenger(Document):
+	def before_save(self):
+        	self.full_name = f'{self.first_name} {self.last_name or ""}'
